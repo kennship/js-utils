c5434164652df9be2f86a668026811608b893724
Ryan Kennedy <r24y@users.noreply.github.com>
2017-06-01 19:23:27 -0400

    eslint-config-kennship: fix `require-jsdoc` rule

--- src/eslint-config-kennship/testing.js
+++ src/eslint-config-kennship/testing.js
@@ -2,7 +2,7 @@ const baseConfig = require('./_base');
 
 module.exports = Object.assign({}, baseConfig, {
   rules: Object.assign({}, baseConfig.rules, {
-    'require-jsdoc': 'never',
+    'require-jsdoc': 'off',
   }),
   env: Object.assign({}, baseConfig.env, {
     mocha: true,
